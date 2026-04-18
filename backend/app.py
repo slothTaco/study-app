c3c475714eea6f918895d84aa96f98548b3a08c4
William Scott Pickering <pickeringwilliamscott@gmail.com>
2026-04-18 07:53:36 -0500

    Prepare Flask backend for deployment and enviroment usage

--- backend/app.py
+++ backend/app.py
@@ -63,4 +63,5 @@ def get_random_flashcards():
     return jsonify(card)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.getenv("PORT", 5000))
+    app.run(host="0.0.0.0", port = port)
